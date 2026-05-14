670c41e03dbe6b2755078bae8e5c4d232344c303
lilasophia1976 <lilasophia2008@gmail.com>
2026-05-14 11:55:53 -0700

    commitall

---
+++ environ/env04.js
@@ -0,0 +1,61 @@
+let colors=[ "Chartreuse", "Purple","Chartreuse","Purple","Chartreuse","Blue",];
+let count= 0; 
+let colorCounter=0;
+
+//you are trapped button
+$("#clickable") .click( function() {
+
+ count= count + 1;
+ $ ("#clickable") .html("you are trapped for " + count+ " "+"years");
+    
+
+});
+
+//dont-click-me-button 
+$("#click-me") .click( function(){
+
+    count= count + 1;
+    colorsCount=count-1;
+
+     $("#click-me") .html("its color time:" +" "+ colors[count]);
+
+    $("#click-me").css( "background-color", colors [count]);
+
+    $("#images").css ("background-color", colors[count+1])
+
+    if (count==4) { 
+        count=0;  
+        console.log("it happened");
+    }
+
+})
+//joke button 
+$("#click-1") .click( function(){
+    $("#click-1") .html("ha ha ha.");
+
+    if (count==2) {
+        count=0;
+        console.log("it happened");
+    }
+})
+
+//good-button
+let color2=["green",];
+let color3=["red",];
+function askNumber(whatNumber){
+    let userNumber = prompt ("pick a # 1-10");
+
+    if (userNumber == whatNumber) {
+        $("#output").html ("You got it!",)
+        $("#output") .css ("background-color", color2)
+    }
+    else {
+        $("#output") .html ("Nooooope");
+        $("#output") .css ("background-color", color3)
+    }
+}
+
+$("#good-button").click(function(){
+    askNumber(7)
+});
+
---
+++ exercises/garden/creature.js
@@ -0,0 +1,18 @@
+
+// add creature to the garden
+$("#crAdd").click( function(){
+
+    // grab the value from the text input and assign it to a variable crName
+    let crName=$("#crName").val();
+    
+    // test in console
+    console.log(crName);
+    
+    // add this name to a div with a bit of html code attached
+    $("#creature-list").append("<div>"+crName+ colors+"</div>");
+
+     $("#crName").val(""); // write the value
+    // $("#crName").val(); // retrieve the value
+
+}); 
+
