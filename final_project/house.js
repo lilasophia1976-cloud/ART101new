60fa52fe069559644696f3f27fd25ff20268aa8d
lilasophia1976 <lilasophia2008@gmail.com>
2026-06-03 04:38:48 -0700

    sunaofjnaskjndfk

--- final_project/house.js
+++ final_project/house.js
@@ -20,5 +20,34 @@ document.getElementById("door3").addEventListener("click", () => {
   document.getElementById("room4").hidden = false;
 });
 
+document.getElementById("turn_back").addEventListener("click", () => {
+  document.getElementById("room2").hidden = true;
+  document.getElementById("room1").hidden = false;
+});
+document.getElementById("door2leave").addEventListener("click", () => {
+  document.getElementById("room3").hidden = true;
+  document.getElementById("room1").hidden = false;
+});
 
 
+
+
+
+let color=["Yellow"]
+
+
+let count=0;
+$("#item").click(function() {
+
+ $("#counter") .html("You found a key!");
+ $("#k1").css ("background-color",color)
+
+});
+
+ function showElement() {
+            element = document.querySelector('#k1');
+            element.style.visibility = 'visible';
+        }
+$("#item").click(function(){
+    $("#k1").showElement
+})
